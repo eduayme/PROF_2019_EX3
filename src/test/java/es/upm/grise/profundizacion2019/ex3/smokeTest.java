5afb7c41153491d6836ccbf7074bcfc68607791a
Eduard Aymerich <eduardaymerich@gmail.com>
2020-01-24 16:02:23 +0100

    refactor tests

--- src/test/java/es/upm/grise/profundizacion2019/ex3/smokeTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex3/smokeTest.java
@@ -21,19 +21,10 @@ public class smokeTest {
 	}
 
 	@Test
-	public void smoleTest() {
-		long SECONDS = 60;
-		my.nextTime(SECONDS);
-	}
-
-	@Test
 	public void add2min_OK() {
 		Time t = new Time();
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 		String date1 = "2020/01/01 00:00:00";
 		String date2 = "2020/01/01 00:02:00";
-		LocalDateTime current_date = LocalDateTime.parse(date1, formatter);
-		LocalDateTime final_date   = LocalDateTime.parse(date2, formatter);
 		long SECONDS = 120;
 
 		assertEquals( t.getFutureTime(SECONDS, date1), date2 );
